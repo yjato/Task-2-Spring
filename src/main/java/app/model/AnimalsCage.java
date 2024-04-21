8aa1e797688af82b5dc4a556eae394d1401aa611
yjato <155980319+yjato@users.noreply.github.com>
2024-04-21 13:13:01 +0700

    Add files via upload

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,27 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+    private Animal animal;
+    private Timer timer;
+
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+}
